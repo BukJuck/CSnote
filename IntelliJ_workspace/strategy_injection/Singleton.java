d48914da3f4b901b6caa7112d15e780721cfe5f0
bukjuck <qjawlsghd123@gmail.com>
2022-11-04 13:27:12 +0900

    이게 맞던가

---
+++ IntelliJ_workspace/strategy_injection/Singleton.java
@@ -0,0 +1,8 @@
+public class Singleton {
+    private static class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+    public static Singleton getInstance(){
+        return singleInstanceHolder.INSTANCE;
+    }
+}
